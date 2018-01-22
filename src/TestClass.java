9a488298e7795cd4c2287bbdba0ab4cb2d27acc1
David Crhonek <crhonek.david@email.cz>
2018-01-22 19:56:02 +0100

    M7P-4thvalid - Fourth valid commit
    M7P-5thbroken - Broken commit method

--- src/TestClass.java
+++ src/TestClass.java
@@ -5,5 +5,11 @@ public class TestClass {
         System.out.println("Second valid commit");
 
         System.out.println("Third invalid commit");
+
+        System.out.println("Fourth valid commit");
+    }
+
+    public static void hello() {
+        System.out.println("I am broken");
     }
 }
